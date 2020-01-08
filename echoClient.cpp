2340c75df848a3742a2867af800f29f982931c23
houchenxi <houchenxi@jd.com>
2020-01-07 22:58:57 -0500

    Revert "update client logic: recv timeout & excep handling & fail retry"
    This reverts commit ac642ea66c269ccddc3bfe89b60738ec2273e3b7.

--- echoClient.cpp
+++ echoClient.cpp
@@ -14,6 +14,7 @@ using namespace ::apache::thrift::concurrency;
 
 using namespace ::echo;
 
+
 int main()
 {
   boost::shared_ptr<TSocket> socket(new TSocket("localhost",9148));
@@ -21,33 +22,17 @@ int main()
   boost::shared_ptr<TProtocol> protocolClient(new TBinaryProtocol(transportClient));
 
   EchoServiceClient client(protocolClient);
-  //socket->setConnTimeout(1);
-  //socket->setSendTimeout(1);
-  socket->setRecvTimeout(1000);
 
-  for(int retry = 0; retry < 2; retry++)
-  {
-    try{
-      transportClient->open();
+  transportClient->open();
 
-      string str;
-      client.Echo(str, "hello");
-      cout << str << endl;
+  string str;
+  client.Echo(str, "hello");
+  cout << str << endl;
 
-      client.Echo(str, "olleh");
-      cout << str << endl;
+  client.Echo(str, "olleh");
+  cout << str << endl;
 
-      transportClient->close();
-      return 0;
-    }
-    catch(apache::thrift::transport::TTransportException& tx){
-      cout << "transport Exception" << "retry #" << retry << " failed" << endl;;
-      transportClient->close();
-    }
-    catch(apache::thrift::TException& tx){
-      cout << "other Exception" << "retry #" << retry << " failed" << endl;
-      transportClient->close();
-    }
-  }
+  transportClient->close();
+
   return 0;
 }
